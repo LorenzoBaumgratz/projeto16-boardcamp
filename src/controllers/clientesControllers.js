f0b7baedf9f22003cf5cb32cf5c4b45ef49c4a71
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-13 12:24:35 -0300

    fix hub putClienteById

--- src/controllers/clientesControllers.js
+++ src/controllers/clientesControllers.js
@@ -35,8 +35,8 @@ export async function putClienteById(req,res){
         const cliente=await db.query(`select * from customers where id=$1;`,[id])
         if(!cliente) return res.sendStatus(404)
 
-        const existeCpf=await db.query(`select * from customers where cpf=$1;`,[cpf])
-        if(existeCpf.rows.length>1) return res.sendStatus(409)
+        const existeCpf=await db.query(`select * from customers where id=$1 and cpf=$1;`,[cpf])
+        if(existeCpf.rows.length===0) return res.sendStatus(409)
 
         const clientePut=await db.query(`update customers set name=$1,phone=$2,cpf=$3,birthday=$4 where id=$5;`,[name,phone,cpf,birthday.split("T"),id])
         res.sendStatus(200)
